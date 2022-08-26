be302831d6af9c529b3bd6d399b79ad3288bc5a9
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-08-26 09:29:02 -0400

    metodos POST implementados

--- projeto11-tweteroo/index.js
+++ projeto11-tweteroo/index.js
@@ -1,12 +1,30 @@
 import express from 'express';
+import cors from 'cors';
+
 
 const server = express();
+server.use(cors());
+server.use(express.json());
 
+let usuario = [];
 let twits = [];
 
+server.post('/sign-up', (req, res) =>{
+	usuario = [...usuario, req.body];
+	res.send('OK');
+})
+
+server.post('/tweets', (req, res) =>{
+	console.log(req.body);
+	twits = [...twits, req.body];
+	console.log(twits);
+	res.send('OK');
+})
 
 server.get('/tweets', (req, res) =>{
-    res.send(twits.slice(twits.length -10));
+	if(twits.length >= 10){
+    	res.send(twits.slice(twits.length -10));
+	} else res.send(twits);
 })
 
 
