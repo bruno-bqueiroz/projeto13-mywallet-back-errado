27324dd5e3a44821129589286f7317730fa78f13
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-08-29 10:49:02 -0400

    validação de dados, status code, parametro de rota e Username por Headers

--- projeto11-tweteroo/index.js
+++ projeto11-tweteroo/index.js
@@ -6,32 +6,57 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-let usuario = [];
+let usuario = [{
+    	username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+		},
+		{
+		username: "patrick",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+		}
+	];
 let tweets = [];
-
+// API DE LOGIN
 server.post('/sign-up', (req, res) =>{
+
+	if (!req.body.username || !req.body.avatar){
+		return res.status(400).send('Todos os Campos são obrigatórios!');
+	}
 	usuario = [...usuario, req.body];
 	console.log(usuario);
-	res.send('OK');
+	res.status(201).send('OK');
 })
 
+// API QUE ARMAZENA OS TWEETS DE TODOS OS USUÁRIOS
 server.post('/tweets', (req, res) =>{
+	if (!req.headers.username || !req.body.tweet){
+		return res.status(400).send('Todos os Campos são obrigatórios!');
+	}
 
-	const avatar = usuario.find((value) => value.username === req.body.username) 
+	const avatar = usuario.find((value) => value.username === req.headers.username) 
 	const twiti = req.body;
 	
-
 	 twiti.avatar = avatar.avatar;
+	 twiti.username = req.headers.username;
 	 tweets = [...tweets, twiti];
 	 console.log(tweets);
-	res.send('OK');
+	res.status(201).send('OK');
 })
 
+// API DE RESPONDE COM OS TWEETS DE TODOS OS USUÁRIOS
 server.get('/tweets', (req, res) =>{
+	
+	if (req.query.USERNAME){
+		const { USERNAME } = req.query;
+		let tweetsDoUsuario = tweets.filter((value) => value.username === USERNAME);
+		return res.send(tweetsDoUsuario);
+		
+	}
 	if(tweets.length >= 10){
     	res.send(tweets.slice(tweets.length -10));
 	} else res.send(tweets);
 })
+
 
 
 server.listen(5000, () => console.log('listen on 5000'));
