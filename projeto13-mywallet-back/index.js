a76fcbdfd0d0c197e70de39d00b23f30cafa77ef
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-09-10 17:14:45 -0400

    todas as rotas implementadas e funcionando

--- projeto13-mywallet-back/index.js
+++ projeto13-mywallet-back/index.js
@@ -33,7 +33,7 @@ server.post ('/cadastro', async (req, res) =>{
             email,
             password: hashPassword,
         });
-        
+        await db.collection('saldo').insertOne({email, valor: 0});
         
     } catch (error) {
         console.error(error);
@@ -72,31 +72,70 @@ server.post ('/', async(req, res) =>{
 
 
 
-server.post('/registros', async (req, res)=>{
+server.post('/registrodeentrada', async (req, res)=>{
     const token = req.headers.authorization?.replace('Bearer ', '');
     const { valor, descricao } = req.body;
     try {
         const user = await db.collection('sessions').find({
         }).toArray();
-        
         const retorno = await user.find((value) => value.token === token)
+        const saldo  = await db.collection('saldo').find({email: retorno.email}).toArray();
+        console.log(saldo[0].valor);
 
-
+        const novoSaldo = saldo[0].valor+parseInt(valor);
         if (!retorno){
             console.log(token);
             console.log('entrou');
             return res.status(404).send('Usuário não encontrado');
         }
-        console.log(retorno.email);
+         console.log(retorno.email);
 
         await db.collection('registros').insertOne({
             email: retorno.email,
             valor,
             descricao,
             type: "entrada",
-            date: dayjs().format('DD/MM')
-        });
-        console.log(user)
+            date: dayjs().format('DD/MM'),
+            });
+
+        await db.collection('saldo').updateOne({email: retorno.email},{$set:{valor: novoSaldo}})
+
+        /* console.log(user) */
+        res.status(201).send(user);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+server.post('/registrodesaida', async (req, res) =>{
+    const token = req.headers.authorization?.replace('Bearer ', '');
+    const { valor, descricao } = req.body;
+    try {
+        const user = await db.collection('sessions').find({
+        }).toArray();
+        const retorno = await user.find((value) => value.token === token)
+        const saldo  = await db.collection('saldo').find({email: retorno.email}).toArray();
+        console.log(saldo[0].valor);
+
+        const novoSaldo = saldo[0].valor-parseInt(valor);
+        if (!retorno){
+            console.log(token);
+            console.log('entrou');
+            return res.status(404).send('Usuário não encontrado');
+        }
+         console.log(retorno.email);
+
+        await db.collection('registros').insertOne({
+            email: retorno.email,
+            valor,
+            descricao,
+            type: "saida",
+            date: dayjs().format('DD/MM'),
+            });
+
+        await db.collection('saldo').updateOne({email: retorno.email},{$set:{valor: novoSaldo}})
+
+        /* console.log(user) */
         res.status(201).send(user);
     } catch (error) {
         console.log(error);
@@ -110,24 +149,23 @@ server.get('/registros', async (req, res) =>{
         const user = await db.collection('sessions').find({
         }).toArray();
 
-        const retorno = await user.find((value) => value.token === token)
-        
+        const retorno = await user.find((value) => value.token === token);
+        const valor = await db.collection('saldo').find({
+            email:retorno.email
+        }).toArray();
+        console.log(valor);
         if (!retorno){
             return res.status(404).send('Usuário não encontrado');
         }
-
         console.log(retorno.email);
-        
-
         const result = await db.collection('registros').find({
             email:retorno.email
         }).toArray();
 
-        const registros = await result.filter((value)=> {
-            value.email === retorno.email
-        })
-        res.send(result);
-        
+
+        const extrato = [ valor, result ];
+
+        res.send(extrato);
     } catch (error) {
         console.error(error);
     }
